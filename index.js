bc1a6534cd4468922247dda1bfa09f82e1e76376
sujal880 <sujaldave880@gmail.com>
2024-06-24 11:34:19 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app=express();
 app.use(express.json());
 app.use(allroutes);
 app.get('/',(req,res)=>{
-    console.log('Attendance Module is Running Properly');
+    res.status(200).json({
+        message:'Server is Properly Running'
+    });
 })
 app.listen(PORT,()=>{
     console.log("Server is Running on PORT"+PORT);

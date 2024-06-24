11d3dc39c773a32ac8268898c4d31014fdaa21f7
sujal880 <sujaldave880@gmail.com>
2024-06-24 09:56:28 +0530

    Some Changes

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT=process.config.SERVER | 4000;
 const app=express();
 app.use(express.json());
 app.use(allroutes);
+app.get('/',(req,res)=>{
+    console.log('Attendance Module is Running Properly');
+})
 app.listen(PORT,()=>{
     console.log("Server is Running on PORT"+PORT);
 })
